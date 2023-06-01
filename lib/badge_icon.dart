40fb2cdaf9d572107ea81ae1cdf1c573b5c19cb8
Philip Johnson <johnson@hawaii.edu>
2023-06-01 08:20:24 -1000

    Update badge_icon.dart

--- lib/badge_icon.dart
+++ lib/badge_icon.dart
@@ -8,34 +8,11 @@ class BadgeIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const Color selectedColor = Colors.black;
-    BadgeDot dot = const BadgeDot(color: selectedColor);
-
-    List<Widget> dots = [];
-    if (level == 1) {
-      dots = [Positioned(left: 8, top: 10, child: dot)];
-    } else if (level == 2) {
-      dots = [
-        Positioned(left: 8, top: 4, child: dot),
-        Positioned(left: 8, top: 16, child: dot)
-      ];
-    } else if (level == 3) {
-      dots = [
-        Positioned(left: 8, top: 0, child: dot),
-        Positioned(left: 8, top: 10, child: dot),
-        Positioned(left: 8, top: 20, child: dot)
-      ];
-    }
-
     return Padding(
-      padding: const EdgeInsets.all(3.0),
-      child: Stack(
-        children: [
-          Chip(label: Text('   $name')),
-          ...dots,
-        ],
-      ),
-    );
+        padding: const EdgeInsets.all(3.0),
+        child: Chip(
+            padding: const EdgeInsets.all(0),
+            label: BadgeLabel(text: name, level: level)));
   }
 }
 
@@ -46,6 +23,35 @@ class BadgeDot extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Icon(Icons.star, color: color, size: 12);
+    return Icon(Icons.star, color: color, size: 10);
+  }
+}
+
+class DotColumn extends StatelessWidget {
+  const DotColumn({Key? key, required this.level}) : super(key: key);
+  final int level;
+  @override
+  Widget build(BuildContext context) {
+    BadgeDot dot = const BadgeDot(color: Colors.black);
+    List<BadgeDot> dotArray = List.filled(level, dot);
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: dotArray);
+  }
+}
+
+class BadgeLabel extends StatelessWidget {
+  const BadgeLabel({Key? key, required this.text, required this.level})
+      : super(key: key);
+  final String text;
+  final int level;
+  @override
+  Widget build(BuildContext context) {
+    final dotColumn = DotColumn(level: level);
+    return Row(mainAxisSize: MainAxisSize.min, children: [
+      dotColumn,
+      Text(' $text'),
+    ]);
   }
 }
